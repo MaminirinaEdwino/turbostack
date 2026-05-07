aca288765c690737048fe62b3c9b794caf0e5942
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-05-07 21:40:29 +0300

    implement: model entity

--- src/entity/model.go
+++ src/entity/model.go
@@ -1,1 +1,35 @@
 package entity
+
+type Model struct {
+	Nom       string   `json:"nom"`
+	Attributs []Champs `json:"attributs"`
+}
+
+func (m *Model) GetNom() string {
+	return m.Nom
+}
+
+func (m *Model) GetAttributs() []Champs {
+	return m.Attributs
+}
+
+func (m *Model) SetNom(nom string) {
+	m.Nom = nom
+}
+
+func (m *Model) SetAttributs(attr []Champs) {
+	m.Attributs = attr
+}
+
+func (m *Model) AddAttribut(attr Champs) {
+	m.Attributs = append(m.Attributs, attr)
+}
+
+func (m *Model) DeleteAttribut(attrId int) {
+	for i := range m.Attributs {
+		if attrId == i {
+			m.Attributs = append(m.Attributs[:i-1], m.Attributs[i:]...)
+		}
+	}
+}
+

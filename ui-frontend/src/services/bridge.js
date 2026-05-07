a11ab67be87e3e29a32a3a53b25095d2ded01e90
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-05-07 20:46:24 +0300

    check: basic function

--- ui-frontend/src/services/bridge.js
+++ ui-frontend/src/services/bridge.js
@@ -1,13 +1,13 @@
 export const GoApp = {
     sayHello: async (name) => {
-        if (window.sayHello) {
+        if (window.SayHello) {
             return await window.sayHello(name);
         }
         // Fallback pour le développement dans un navigateur classique
         return `Mode démo : Bonjour ${name}`;
     },
     getStats: async () => {
-        if (window.getStats) {
+        if (window.GetStats) {
             return await window.getStats();
         }
         return { os: "Browser", arch: "wasm" };

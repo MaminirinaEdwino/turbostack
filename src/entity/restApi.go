032f042c41bfa55336075163b3143cb71928fe20
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-05-08 08:46:36 +0300

    implement: restapi entity

--- src/entity/restApi.go
+++ src/entity/restApi.go
@@ -1,1 +1,34 @@
 package entity
+
+type RestApi struct {
+	Endpoints []Endpoint
+	Role []string
+	BDD BDD
+}
+
+func (restapi *RestApi) GetEndpoints() []Endpoint {
+	return restapi.Endpoints
+}
+
+func (restapi *RestApi) GetRole() []string {
+	return restapi.Role
+}
+
+func (restapi *RestApi) GetBDD() BDD {
+	return restapi.BDD
+}
+
+func (restapi *RestApi) SetEndpoints(Endpoints []Endpoint) *RestApi {
+	restapi.Endpoints = Endpoints
+	return restapi
+}
+
+func (restapi *RestApi) SetRole(Role []string) *RestApi {
+	restapi.Role = Role
+	return restapi
+}
+
+func (restapi *RestApi) SetBDD(BDD BDD) *RestApi {
+	restapi.BDD = BDD
+	return restapi
+}

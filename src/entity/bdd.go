3e521ac8c3203cc8f57f3bc04d0180965430820d
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-05-08 08:15:37 +0300

    implement: model function

--- src/entity/bdd.go
+++ src/entity/bdd.go
@@ -21,4 +21,17 @@ func (db *BDD) SetSGBD(sgbd string) {
 	db.SGBD = sgbd
 }
 
+func (db *BDD) AddModel(model Model) []Model {
+	db.Models = append(db.Models, model)
+	return db.Models
+}
+
+func (db *BDD) DeleteModel(model_id int) []Model {
+	for i := range db.Models {
+		if i == model_id {
+			db.Models = append(db.Models[:i-1], db.Models[i:]...)
+		}
+	}
+	return db.Models
+}
 

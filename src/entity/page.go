64616327fa8a3e9a70ccf9c71a92b65118affdfa
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-05-08 08:38:29 +0300

    implement: Page entity

--- src/entity/page.go
+++ src/entity/page.go
@@ -1,1 +1,24 @@
 package entity
+
+type Page struct {
+	Nom     string
+	Contenu []any
+}
+
+func (page *Page) GetNom() string {
+	return page.Nom
+}
+
+func (page *Page) GetContenu() any {
+	return page.Contenu
+}
+
+func (page *Page) SetNom(Nom string) *Page {
+	page.Nom = Nom
+	return page
+}
+
+func (page *Page) SetContenu(Contenu []any) *Page {
+	page.Contenu = Contenu
+	return page
+}

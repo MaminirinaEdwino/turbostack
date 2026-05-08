31956b2ee9865ce10ee64170089acf42060930cc
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-05-08 09:14:20 +0300

    implement: project and project manager

--- src/entity/project.go
+++ src/entity/project.go
@@ -1,1 +1,64 @@
 package entity
+
+type Project struct {
+	Nom          string
+	Type         string
+	BDD          BDD
+	RestApi      RestApi
+	WebApp       WebApp
+	SiteStatique SiteStatique
+}
+
+func (project *Project) GetNom() string {
+	return project.Nom
+}
+
+func (project *Project) GetType() string {
+	return project.Type
+}
+
+func (project *Project) GetBDD() BDD {
+	return project.BDD
+}
+
+func (project *Project) GetRestApi() RestApi {
+	return project.RestApi
+}
+
+func (project *Project) GetWebApp() WebApp {
+	return project.WebApp
+}
+
+func (project *Project) GetSiteStatique() SiteStatique {
+	return project.SiteStatique
+}
+
+func (project *Project) SetNom(Nom string) *Project {
+	project.Nom = Nom
+	return project
+}
+
+func (project *Project) SetType(Type string) *Project {
+	project.Type = Type
+	return project
+}
+
+func (project *Project) SetBDD(BDD BDD) *Project {
+	project.BDD = BDD
+	return project
+}
+
+func (project *Project) SetRestApi(RestApi RestApi) *Project {
+	project.RestApi = RestApi
+	return project
+}
+
+func (project *Project) SetWebApp(WebApp WebApp) *Project {
+	project.WebApp = WebApp
+	return project
+}
+
+func (project *Project) SetSiteStatique(SiteStatique SiteStatique) *Project {
+	project.SiteStatique = SiteStatique
+	return project
+}
--- src/entity/projectManger.go
+++ src/entity/projectManger.go
@@ -1,1 +1,55 @@
 package entity
+
+import (
+	"encoding/json"
+	"os"
+	"slices"
+)
+
+type ProjectManager struct {
+	Projects []Project
+}
+
+func (mgr *ProjectManager) LoadProjects() error {
+	file, err := os.ReadFile("project.json")
+	if err != nil {
+		return err
+	}
+	json.Unmarshal(file, &mgr.Projects)
+	return nil
+}
+
+func (mgr *ProjectManager) GetAll() []Project {
+	return mgr.Projects
+}
+
+func (mgr *ProjectManager) Create(project Project) {
+	mgr.Projects = append(mgr.Projects, project)
+}
+
+func (mgr *ProjectManager) Update(project Project) {
+	for i, val := range mgr.Projects {
+		if val.GetNom() == project.GetNom() {
+			mgr.Projects[i] = project
+			break
+		}
+	}
+}
+
+func (mgr *ProjectManager) Delete(project Project) {
+	for i, val := range mgr.Projects {
+		if val.GetNom() == project.GetNom() {
+			mgr.Projects = slices.Delete(mgr.Projects, i, i+1)
+			break
+		}
+	}
+}
+
+func (mgr *ProjectManager) GetByName(name string) Project {
+	for _, val := range mgr.Projects {
+		if val.GetNom() == name {
+			return val
+		}
+	}
+	return Project{}
+}

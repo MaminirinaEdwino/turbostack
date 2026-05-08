346586b9f0ae3c802046f7bb462f933c76e071b0
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-05-08 08:40:18 +0300

    implement: site statique entity

--- src/entity/siteStatique.go
+++ src/entity/siteStatique.go
@@ -1,1 +1,23 @@
 package entity
+
+type SiteStatique struct {
+	Pages []Page
+	Composants []Composant
+}
+func (sitestatique *SiteStatique) GetPages() []Page {
+	return sitestatique.Pages
+}
+
+func (sitestatique *SiteStatique) GetComposants() []Composant {
+	return sitestatique.Composants
+}
+
+func (sitestatique *SiteStatique) SetPages(Pages []Page) *SiteStatique {
+	sitestatique.Pages = Pages
+	return sitestatique
+}
+
+func (sitestatique *SiteStatique) SetComposants(Composants []Composant) *SiteStatique {
+	sitestatique.Composants = Composants
+	return sitestatique
+}

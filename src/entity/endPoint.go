13ceb947ea46b190c3878a838f617360661996da
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-05-08 08:28:40 +0300

    implement: endpoint entity

--- src/entity/endPoint.go
+++ src/entity/endPoint.go
@@ -1,1 +1,58 @@
 package entity
+
+type Endpoint struct {
+	Nom    string
+	Uri    string
+	Method string
+	Model  []Model
+	Params []string
+	Role   string
+}
+
+func (e *Endpoint) GetNom() string {
+	return e.Nom
+}
+
+func (e *Endpoint) GetUri() string {
+	return e.Uri
+}
+
+func (e *Endpoint) GetMethod() string {
+	return e.Method
+}
+
+func (e *Endpoint) GetModel() []Model {
+	return e.Model
+}
+
+func (e *Endpoint) GetParams() []string {
+	return e.Params
+}
+
+func (e *Endpoint) GetRole() string {
+	return e.Role
+}
+
+func (e *Endpoint) SetNom(Nom string) {
+	e.Nom = Nom
+}
+
+func (e *Endpoint) SetUri(Uri string) {
+	e.Uri = Uri
+}
+
+func (e *Endpoint) SetMethod(Method string) {
+	e.Method = Method
+}
+
+func (e *Endpoint) SetModel(Model []Model) {
+	e.Model = Model
+}
+
+func (e *Endpoint) SetParams(Params []string) {
+	e.Params = Params
+}
+
+func (e *Endpoint) SetRole(Role string) {
+	e.Role = Role
+}

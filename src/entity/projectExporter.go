e95b790a59a105af9ef22df5c2ebde25d1e65e21
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-05-16 11:29:33 +0300

    clean

--- src/entity/projectExporter.go
+++ src/entity/projectExporter.go
@@ -4,21 +4,14 @@ import "fmt"
 
 func (mgr *ProjectManager) ExporterDB(Project Project) {
 	fmt.Println("Database")
-	fmt.Println(Project.GetBDD())
 }
 
 func (mgr *ProjectManager) ExporterAPI(Project Project) {
 	fmt.Println("Rest APi")
-	fmt.Println(Project.GetBDD())
-	fmt.Println(Project.GetRestApi())
 }
 func (mgr *ProjectManager) ExporterStaticSite(Project Project) {
 	fmt.Println("static site")
-	fmt.Println(Project.GetSiteStatique())
 }
 func (mgr *ProjectManager) ExporterWebApp(Project Project) {
 	fmt.Println("web app")
-	fmt.Println(Project.GetBDD())
-	fmt.Println(Project.GetBDD())
-	fmt.Println(Project.GetWebApp())
 }

8038f742704dd75802a52e9a601fd865a0022490
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-05-16 08:05:29 +0300

    feat: last project loading

--- ui-frontend/src/appSlice.js
+++ ui-frontend/src/appSlice.js
@@ -1,25 +1,32 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const lastProject = localStorage.getItem('turbostack_project') || "";
+const lastWindow = localStorage.getItem('turbostack_window') || 'Dashboard';
+const lastDarkMode = localStorage.getItem('turbostack_darkMode') === 'true';
+
 const appSlice = createSlice({
   name: 'app',
   initialState: {
-    actualWindow: 'Dashboard', // La fenêtre par défaut au démarrage de l'application
-    actualProject: "",
+    actualWindow: lastWindow,
+    actualProject: lastProject,
     toggleMenuSide: false,
-    darkMode: false,
+    darkMode: lastDarkMode,
   },
   reducers: {
     setActualWindow: (state, action) => {
       state.actualWindow = action.payload;
+      localStorage.setItem('turbostack_window', action.payload);
     },
     setActualProject: (state, action) => {
       state.actualProject = action.payload;
+      localStorage.setItem('turbostack_project', action.payload);
     },
     setToggleMenuSide: (state) => {
       state.toggleMenuSide = !state.toggleMenuSide;
     },
     setToggleDarkMode: (state) => {
-      state.darkMode = !state.darkMode
+      state.darkMode = !state.darkMode;
+      localStorage.setItem('turbostack_darkMode', state.darkMode);
     }
   },
 });

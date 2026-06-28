3848d52fc3658d0610c7afd21a34a380dc74d093
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-06-28 20:05:53 +0300

    fix: separate select node

--- ui-frontend/src/layouts/projects/endPointEditor/customNode.jsx
+++ ui-frontend/src/layouts/projects/endPointEditor/customNode.jsx
@@ -3,33 +3,7 @@ import { Handle, Position } from "@xyflow/react";
 import { Play, Plug, Plus } from "lucide-react";
 
 
-export function SelectNode({ id, data, isConnectable, selectType }) {
-  const onChange = (field, value) => {
-    if (data.onNodeDataChange) {
-      data.onNodeDataChange(id, { ...data, [field]: value });
-    }
-  };
-  return (
-    <div className="p-2 bg-gray-900 rounded-md min-w-40 border border-couleur2 font-mono" >
-      <div className="text-center mb-2 text-white/50  border-b border-couleur2"> GET { selectType }</div>
-      <Handle
-        type="target"
-        position={Position.Left}
-        isConnectable={isConnectable}
-        style={{ background: "#f38ba8" }}
-      />
-      <div className="flex justify-center text-couleur2">
-        <button className="flex justify-center gap-1 items-center text-[10px]" onClick={()=>data.addChildAutomatically(id, "whereNode")}> <Plug size={10}/> WHERE </button>
-      </div>
-      <Handle
-        type="source"
-        position={Position.Right}
-        isConnectable={isConnectable}
-        style={{ background: "#f38ba8" }}
-      />
-    </div>
-  );
-}
+
 // 2. NŒUD VARIABLE (ex: Payload d'entrée, Variable locale d'API)
 export function VarNode({ id, data, isConnectable }) {
   const onChange = (field, value) => {

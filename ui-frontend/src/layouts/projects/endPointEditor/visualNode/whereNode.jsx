dc0044adcd39e422c01502f254313024dd8b0ff2
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-06-28 20:04:51 +0300

    fix: separate where node

---
+++ ui-frontend/src/layouts/projects/endPointEditor/visualNode/whereNode.jsx
@@ -0,0 +1,26 @@
+import { Position } from "@xyflow/react";
+
+export function WhereNode({ id, data, isConnectable }) {
+  // const onChange = (field, value) => {
+  //   if (data.onNodeDataChange) {
+  //     data.onNodeDataChange(id, { ...data, [field]: value });
+  //   }
+  // };
+  return (
+    <div className="p-2 bg-gray-900 rounded-md min-w-40 border border-couleur2 font-mono">
+      <div className="flex justify-center text-white/50 border-b border-couleur2 mb-2">WHERE</div>
+      <Handle
+        type="target"
+        position={Position.Left}
+        isConnectable={isConnectable}
+        style={{ background: "#f38ba8" }}
+      />
+      <Handle
+        type="source"
+        position={Position.Right}
+        isConnectable={isConnectable}
+        style={{ background: "#f38ba8" }}
+      />
+    </div>
+  );
+}

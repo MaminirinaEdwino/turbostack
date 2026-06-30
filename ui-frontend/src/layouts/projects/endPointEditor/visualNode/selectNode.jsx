e8d57c69756607cd55184a0d4d4e787086e0e029
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-06-30 20:59:24 +0300

    feat: where node, condition choice

--- ui-frontend/src/layouts/projects/endPointEditor/visualNode/selectNode.jsx
+++ ui-frontend/src/layouts/projects/endPointEditor/visualNode/selectNode.jsx
@@ -1,13 +1,13 @@
 import { Handle, Position } from "@xyflow/react";
 import { Plug } from "lucide-react";
 
-export default  function SelectNode({ id, data, isConnectable }) {
+export default function SelectNode({ id, data, isConnectable }) {
   const onChange = (field, value) => {
     if (data.onNodeDataChange) {
       data.onNodeDataChange(id, { ...data, [field]: value });
     }
   };
-  let otherData = {}
+
   return (
     <div className="p-2 bg-gray-900 rounded-md min-w-40 border border-couleur2 font-mono" >
       <div className="text-center mb-2 text-white/50  border-b border-couleur2"> GET {data.selectedType} FROM {data.model.nom}</div>
@@ -21,7 +21,7 @@ export default  function SelectNode({ id, data, isConnectable }) {
         style={{ background: "#f38ba8" }}
       />
       <div className="flex justify-center text-couleur2">
-        <button className="flex justify-center gap-1 items-center text-[10px]" onClick={()=>data.addChildAutomatically(id, "whereNode", otherData)}> <Plug size={10}/> WHERE </button>
+        <button className="flex justify-center gap-1 items-center text-[10px]" onClick={()=>data.addChildAutomatically(id, "whereNode", {selectedType: data.selectedType, model: data.model})}> <Plug size={10}/> WHERE </button>
       </div>
       <Handle
         type="source"
--- ui-frontend/src/layouts/projects/endPointEditor/visualNode/whereNode.jsx
+++ ui-frontend/src/layouts/projects/endPointEditor/visualNode/whereNode.jsx
@@ -1,11 +1,11 @@
 import { Handle, Position } from "@xyflow/react";
 
 export default function WhereNode({ id, data, isConnectable }) {
-  // const onChange = (field, value) => {
-  //   if (data.onNodeDataChange) {
-  //     data.onNodeDataChange(id, { ...data, [field]: value });
-  //   }
-  // };
+  const onChange = (field, value) => {
+    if (data.onNodeDataChange) {
+      data.onNodeDataChange(id, { ...data, [field]: value });
+    }
+  };
   return (
     <div className="p-2 bg-gray-900 rounded-md min-w-40 border border-couleur2 font-mono">
       <div className="flex justify-center text-white/50 border-b border-couleur2 mb-2">WHERE</div>
@@ -15,6 +15,33 @@ export default function WhereNode({ id, data, isConnectable }) {
         isConnectable={isConnectable}
         style={{ background: "#f38ba8" }}
       />
+      <div className="text-couleur2"> {data.model.nom}
+        {data.model.champs.map((field) => <>
+          <div>
+            <input
+            className="hidden"
+              type="checkbox"
+              onChange={(e) => {
+                onChange(data.model.nom+"_check_" + field.nom, e.target.checked)
+              }}
+              id={data.model.nom + "_where_for_" + data.model.nom + "" + field.nom}
+            />
+            <label htmlFor={data.model.nom + "_where_for_" + data.model.nom + "" + field.nom}
+              className={data[data.model.nom+"_check_" + field.nom] && "border-b"}>  {field.nom} </label>
+            {
+              data[data.model.nom + "_check_" + field.nom]  && <div>
+                <select className="" onChange={(e)=>onChange(data.model.nom+"_operator_"+field.nom, e.target.value)} >
+                  <option value="=">=</option>
+                  <option value=">=">=</option>
+                  <option value="<="> {"<="} </option>
+                  <option value="!=">!=</option>
+                </select>
+                <input className="bg-couleur2 rounded-sm mx-1 text-couleur1 px-1" type="text" onChange={(e) => onChange(data.model.nom + "_where_" + field.nom, e.target.value)}  />
+              </div>
+            }
+          </div>
+        </>)}
+      </div>
       <Handle
         type="source"
         position={Position.Right}

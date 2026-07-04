931ee376713e20bad610f2b39bb7f8d56dfbfa73
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-07-04 12:05:53 +0300

    fix: responseNode parent

--- ui-frontend/src/layouts/projects/endPointEditor/visualNode/responseNode.jsx
+++ ui-frontend/src/layouts/projects/endPointEditor/visualNode/responseNode.jsx
@@ -10,14 +10,19 @@ export default function ResponseNode({ id, data, isConnectable }) {
       data.onNodeDataChange(id, { ...data, [field]: value });
     }
   };
-  return <div className=" text-couleur2 border border-couleur2 rounded-md">
+  return <div className=" text-couleur2 border border-couleur2 rounded-md bg-gray-900 min-w-40">
 
-
+    <Handle
+      type="target"
+      position={Position.Top}
+      isConnectable={isConnectable}
+      id={"connect_from_parent"}
+    />
     <h3 className="text-white/50 border-b border-couleur2 m-2 flex justify-between">Response
       <button onClick={()=>setAddResponse(!addResponse)}><Plus/></button>
     </h3>
     <div className="relative flex mb-2">
-      <span className="px-2 text-[12px]">Status</span>
+      <span className="px-2 text-[12px]">Status Code</span>
       <Handle
         type="target"
         id={"response_status"}
@@ -34,13 +39,14 @@ export default function ResponseNode({ id, data, isConnectable }) {
       <span className="mx-2 text-[12px]">{res}</span>
     </div>)}
     {addResponse && <div className="p-2 flex items-center">
-      <input type="text" placeholder="field name" value={fieldName} onChange={(e)=>setFieldName(e.target.value)} className="border-b outline-couleur2"/>
+      <input type="text" placeholder="field name" value={fieldName} onChange={(e)=>setFieldName(e.target.value)} className="border-b outline-couleur2 px-2 py-1 text-[10px]"/>
       <button className="p-1" onClick={() => {
         if (data.response) {
           let list = data["response"]
           list.push(fieldName)
           onChange("response", list)
           setFieldName("")
+          setAddResponse(!addResponse)
         }
       }}><Check/></button>
     </div>}

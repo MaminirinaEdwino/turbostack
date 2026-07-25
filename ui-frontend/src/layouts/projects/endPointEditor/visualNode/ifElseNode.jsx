648d48a3695dbf774438761e82cb088c75f3f4ce
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-07-25 20:18:30 +0300

    add: new blocstyle

--- ui-frontend/src/layouts/projects/endPointEditor/visualNode/ifElseNode.jsx
+++ ui-frontend/src/layouts/projects/endPointEditor/visualNode/ifElseNode.jsx
@@ -2,9 +2,9 @@ import { Handle, Position } from "@xyflow/react";
 import { Plus } from "lucide-react";
 
 export default function IfElseNode({ id, data, isConnectable }) {
-    return <div className="bg-gray-900 border border-couleur2 rounded-md min-w-40 font-mono">
-        <div className="relative">
-            <h2 className="p-2 border-b border-couleur2 mx-2 text-center text-white/50">Condition</h2>
+    return <div className="blocNode2">
+        <div className="">
+            <h2 className="">Condition</h2>
             <Handle
                 type="target"
                 position={Position.Left}
@@ -12,8 +12,8 @@ export default function IfElseNode({ id, data, isConnectable }) {
                 id={'if_else_target_handle'}
             />
         </div>
-        <div className="relative ">
-            <h3 className="text-couleur2 p-4 flex justify-between">If  <button className="cursor-pointer" onClick={() => data.addChildAutomatically(id, "ifNode", {})}> <Plus size={14} /> </button></h3>
+        <div className=" ">
+            <h3 className="">If  <button className="cursor-pointer" onClick={() => data.addChildAutomatically(id, "ifNode", {})}> <Plus size={14} /> </button></h3>
             <Handle
                 type="source"
                 id={"if_handle"}
@@ -22,8 +22,8 @@ export default function IfElseNode({ id, data, isConnectable }) {
             />
 
         </div>
-        <div className="relative">
-            <h3 className="text-couleur2 p-4 flex justify-between">Else If <button className="cursor-pointer" onClick={() => data.addChildAutomatically(id, "elseIfNode", { "sourceHandle": "else_if_handle" })}> <Plus size={14} /> </button></h3>
+        <div className="">
+            <h3 className="">Else If <button className="cursor-pointer" onClick={() => data.addChildAutomatically(id, "elseIfNode", { "sourceHandle": "else_if_handle" })}> <Plus size={14} /> </button></h3>
             <Handle
                 type="source"
                 id={"else_if_handle"}
@@ -32,8 +32,8 @@ export default function IfElseNode({ id, data, isConnectable }) {
             />
 
         </div>
-        <div className="relative">
-            <h3 className="text-couleur2 p-4 flex justify-between">Else <button className="cursor-pointer" onClick={() => data.addChildAutomatically(id, "elseNode", { "sourceHandle": "else_handle" })}> <Plus size={14} /> </button></h3>
+        <div className="">
+            <h3 className="">Else <button className="cursor-pointer" onClick={() => data.addChildAutomatically(id, "elseNode", { "sourceHandle": "else_handle" })}> <Plus size={14} /> </button></h3>
             <Handle
                 type="source"
                 id={"else_handle"}
--- ui-frontend/src/layouts/projects/endPointEditor/visualNode/rootNode.jsx
+++ ui-frontend/src/layouts/projects/endPointEditor/visualNode/rootNode.jsx
@@ -2,7 +2,7 @@ import { Handle, Position } from "@xyflow/react";
 import { Play } from "lucide-react";
 
 export default function RootNode({ isConnectable }) {
-  return <div className="p-2 border border-couleur2 bg-couleur1 rounded-md text-couleur2 ">
+  return <div className="epBlocNode">
     <Play size={14} className="" />
     <Handle
       type="source"

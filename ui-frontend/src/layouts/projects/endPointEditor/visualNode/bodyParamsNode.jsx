43d050f12bdf46152f38ddd2b048bb6128a3c71e
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-07-25 20:04:11 +0300

    fix: body params bode style

--- ui-frontend/src/layouts/projects/endPointEditor/visualNode/bodyParamsNode.jsx
+++ ui-frontend/src/layouts/projects/endPointEditor/visualNode/bodyParamsNode.jsx
@@ -6,13 +6,13 @@ export default function BodyParamsNode({ id, data, isConnectable }) {
   //     data.onNodeDataChange(id, { ...data, [field]: value });
   //   }
   // };
-  return <div key={id} className="bg-gray-900 p-2 border border-couleur2 rounded-md text-couleur2">
+  return <div key={id} className="epBlocNode">
     <Handle
       position={Position.Right}
       type="source"
       isConnectable={isConnectable}
     />
-    <div className="text-[10px] border-b border-couleur2 mb-2">Request Body params</div>
+    <div className="header">Request Body params</div>
     <div className="text-[10px]">{data.bodyParams.field.nom}: {data.bodyParams.field.type} {data.bodyParams.field.default_value}</div>
   </div>
 }

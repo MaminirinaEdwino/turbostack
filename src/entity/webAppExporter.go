589c9d9f1e4ab95eadbc5c839465011f70a79368
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-08-01 19:49:06 +0300

    fix:

--- src/entity/webAppExporter.go
+++ src/entity/webAppExporter.go
@@ -3,9 +3,15 @@ package entity
 type webAppMaker struct{}
 
 func (wap *webAppMaker) SetupArch() {
-
+	folderList := []string{
+		"src/views",
+		"src/controller",
+		"src/models",
+	}
 }
 
-func (wap *webAppMaker) WebAppGenerator() {
+func (wap *webAppMaker) GenerateView() {}
 
+func (wap *webAppMaker) WebAppGenerator() {
+	wap.SetupArch()
 }

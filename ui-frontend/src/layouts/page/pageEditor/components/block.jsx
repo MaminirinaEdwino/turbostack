46bacdef9f472b2990bf1d9d366b8987ce784332
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-08-22 19:23:38 +0300

    add: some title

--- ui-frontend/src/layouts/page/pageEditor/components/block.jsx
+++ ui-frontend/src/layouts/page/pageEditor/components/block.jsx
@@ -1,7 +1,7 @@
 import { GripVertical } from "lucide-react";
 
 export default function Block({ block, getIconForTag }) {
-    return <div className="flex items-center gap-3">
+    return <div className="flex items-center gap-3" title={`${block.htmlId != "" ? "#"+block.htmlId : block.tag} ${block.inputType != "" ? block.inputType : ""}`}>
         <div className="cursor-grab active:cursor-grabbing text-couleur1/20 group-hover:text-couleur1/60 transition-colors" onClick={(e) => e.stopPropagation()}>
             <GripVertical size={14} />
         </div>
@@ -10,11 +10,11 @@ export default function Block({ block, getIconForTag }) {
         </div>
         <div title={block.htmlId != "" ? "#"+block.htmlId : block.tag}>
             <p className="text-[10px] font-black text-couleur1 dark:text-gray-400 uppercase tracking-tighter w-17 truncate">
-                {block.htmlId != "" ? "#"+block.htmlId : block.tag}
+                {block.htmlId != "" ? "#"+block.htmlId : block.tag} <span className="text-[8px]"> {block.inputType != "" && block.inputType}</span>
             </p>
             <p className="text-[11px] opacity-40 font-mono truncate  w-15 ">
                 {block.tag != "input" && block.content != "" && block.content}
-                {block.tag =="input" && block.placeholder != "" && ( block.inputType && block.inputType  == "submit" || block.inputType =="reset" ? block.inputType : block.placeholder)}
+                {(block.tag =="input" && block.placeholder != "") && ( block.inputType && block.inputType  == "submit" || block.inputType =="reset" ? block.inputType : block.placeholder)}
                 {/* {block.tag =="input" && block.placeholder == "" && block.inpputType == "submit" || block.inputType == "reset" && block.inpputType} */}
             </p>
         </div>

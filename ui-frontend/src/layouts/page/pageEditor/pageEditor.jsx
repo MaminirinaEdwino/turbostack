80fe67e74d1f87a0e843bb5acf3f9f569161886e
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-09-06 10:51:22 +0300

    fix: separate toadt component

--- ui-frontend/src/layouts/page/pageEditor/pageEditor.jsx
+++ ui-frontend/src/layouts/page/pageEditor/pageEditor.jsx
@@ -26,6 +26,7 @@ import DarkModeToggle from "../../../components/darkModeToggle";
 import ToDashBoardBtn from "./components/toDashBoardbtn";
 import Header from "./components/header";
 import PreviewSection from "./components/previewSection";
+import Toast from "../../../components/toast";
 
 export default function PageEditor({ projectName }) {
   const navigateTo = useNavigate();
@@ -593,23 +594,7 @@ export default function PageEditor({ projectName }) {
 
       {/* Toast Notification */}
       {toast && (
-        <div
-          className={`fixed bottom-10 right-10 z-100 flex items-center gap-3 px-5 py-3 rounded-lg shadow-2xl transition-all duration-300 border ${toast.type === "error"
-            ? "bg-red-50 border-red-200 text-red-700"
-            : toast.type === "loading"
-              ? "bg-blue-50 border-blue-200 text-blue-700"
-              : "bg-green-50 border-green-200 text-green-700"
-            }`}
-        >
-          {toast.type === "loading" ? (
-            <Loader2 size={18} className="animate-spin" />
-          ) : toast.type === "error" ? (
-            <AlertCircle size={18} />
-          ) : (
-            <CheckCircle size={18} />
-          )}
-          <span className="font-medium text-sm">{toast.message}</span>
-        </div>
+        <Toast toast={toast} />
       )}
     </div>
   );
